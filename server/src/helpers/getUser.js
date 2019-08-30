07efbc158e693aeab4da61430e444cf45881ac9c
Joao Coelho <joaocoelho93@gmail.com>
2019-08-29 22:49:24 -0400

    Friendlier error when user is not found in the database

--- server/src/helpers/getUser.js
+++ server/src/helpers/getUser.js
@@ -3,6 +3,10 @@ import util from 'util';
 const readFile = util.promisify(fs.readFile);
 
 export default async function getUser(id) {
-  const data = await readFile(`./data/users/${id}.json`, 'utf8');
+  try {
+    const data = await readFile(`./data/users/${id}.json`, 'utf8');
+  } catch (e) {
+    throw new Error('User Not Found');
+  }
   return JSON.parse(data);
 }
