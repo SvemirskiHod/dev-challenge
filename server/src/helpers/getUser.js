2688f52fd2d07e236d3ed61ccff91657b713c8fa
Joao Coelho <joaocoelho93@gmail.com>
2019-08-30 00:18:10 -0400

    Implemented a user cache to speed up user querying

--- server/src/helpers/getUser.js
+++ server/src/helpers/getUser.js
@@ -1,13 +1,20 @@
 import fs from 'fs';
 import util from 'util';
+import userCache from './userCache.js';
+
 const readFile = util.promisify(fs.readFile);
 
 export default async function getUser(id) {
+  if (userCache[id]) {
+    return userCache[id];
+  }
   let data;
   try {
     data = await readFile(`./data/users/${id}.json`, 'utf8');
   } catch (e) {
     throw new Error('User Not Found');
   }
-  return JSON.parse(data);
+  const parsed = JSON.parse(data);
+  userCache[id] = parsed;
+  return parsed;
 }
--- server/src/helpers/setUser.js
+++ server/src/helpers/setUser.js
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import util from 'util';
+import userCache from './userCache';
+
 const writeFile = util.promisify(fs.writeFile);
 
 export default async function setUser(user) {
+  userCache[user.id] = user;
   return writeFile(`./data/users/${user.id}.json`, JSON.stringify(user));
 }
