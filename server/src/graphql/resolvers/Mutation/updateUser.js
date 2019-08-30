de8d631ff7d8e0622e420c74dfe2c8016e4b08c9
Joao Coelho <joaocoelho93@gmail.com>
2019-08-29 22:38:55 -0400

    Can now update user without using other data

--- server/src/graphql/resolvers/Mutation/updateUser.js
+++ server/src/graphql/resolvers/Mutation/updateUser.js
@@ -1,13 +1,14 @@
-import { setUser } from '../../../helpers';
+import { setUser, getUser } from '../../../helpers';
 
 export default async function User(root, { user }, { ctx }, info) {
   // todo: 1 this throws a unfriendly (and potentially unsafe) error if a non-existnant user ID is entered.
   // how can we check for a non-existing user id and throw a more friendly error.
 
-  // todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
+  // DONE - todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
   // updated rather than overwritting all the data.
-
-  await setUser(user);
+  const userInDatabase = await getUser(user.id);
+  const updatePayload = Object.assign({}, userInDatabase, user);
+  await setUser(updatePayload);
 
   return true;
 }
