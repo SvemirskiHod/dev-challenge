e7914ef7632781a3faef7baadcc791702fa7efe0
Joao Coelho <joaocoelho93@gmail.com>
2019-08-29 22:52:38 -0400

    Fixed bug with user fetching

--- server/src/helpers/getUser.js
+++ server/src/helpers/getUser.js
@@ -3,8 +3,9 @@ import util from 'util';
 const readFile = util.promisify(fs.readFile);
 
 export default async function getUser(id) {
+  let data;
   try {
-    const data = await readFile(`./data/users/${id}.json`, 'utf8');
+    data = await readFile(`./data/users/${id}.json`, 'utf8');
   } catch (e) {
     throw new Error('User Not Found');
   }
