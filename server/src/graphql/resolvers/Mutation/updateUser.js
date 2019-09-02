23e49c014a6c685d47f114ce8121f12524df5eb3
Joao Coelho <joaocoelho93@gmail.com>
2019-09-02 19:36:21 -0400

    added comment for clarity

--- server/src/graphql/resolvers/Mutation/updateUser.js
+++ server/src/graphql/resolvers/Mutation/updateUser.js
@@ -4,7 +4,7 @@ export default async function User(root, { user }, { ctx }, info) {
   // DONE - todo: 1 this throws a unfriendly (and potentially unsafe) error if a non-existnant user ID is entered.
   // how can we check for a non-existing user id and throw a more friendly error.
 
-  // DONE - todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
+  // NOTE: Changes made to the setUser helper DONE - todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
   // updated rather than overwritting all the data.
   await setUser(user);
   return true;
